99c89e6a27ae939096d8935f5fd9d8c7d0de5201
Eddie Gieze <eddie@Uptrends.com>
2019-11-09 09:07:38 +0100

    hackathon result

--- src/logic/trello-service.ts
+++ src/logic/trello-service.ts
@@ -1,5 +1,6 @@
-import { TrelloClient } from "trello.ts";
-import { TrelloApiKey, TrelloToken } from "../config";
+import { TrelloClient } from 'trello.ts';
+import { TrelloApiKey, TrelloToken } from '../config';
+import { Board } from 'trello.ts/out/api';
 
 export class TrelloService {
     private trelloClient: TrelloClient;
@@ -7,13 +8,26 @@ export class TrelloService {
     constructor() {
         this.trelloClient = new TrelloClient({
             key: TrelloApiKey,
-            token: TrelloToken,
+            token: TrelloToken
         });
     }
 
     public getBoards() {
         return this.trelloClient.organizations.getBoards({
-            id: "dreamteam593",
+            id: 'dreamteam593'
         });
+    }
+
+    public async getCardCount (boardId:string) : Promise<any[]> {
+        const lists = await this.trelloClient.board.getLists({ id: boardId })
+        let listCounts:any[] = [];
+        for (const list of lists) {
+            //console.log(list)
+            
+            const allCards = await this.trelloClient.lists.getCards({ id: list.id})
+            listCounts.push({name: list.name, cardCount: allCards.length})
+        }
+        console.log(listCounts)
+        return listCounts;
     }
 }
